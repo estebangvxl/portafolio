28a9b3ff1cfc44510f4a5eba70b0a7c9a2268404
estebangvx <estebangvx@gmail.com>
2024-11-11 22:23:33 -0500

    se agregó proyecto pokemon en lista de proyectos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -84,7 +84,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/estebangvxl/portafolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
